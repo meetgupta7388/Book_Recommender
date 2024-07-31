fffcbe5d675cceca4462531e62d703d1c55127c3
meetgupta7388 <137211709+meetgupta7388@users.noreply.github.com>
2024-07-31 18:29:32 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+
+popular_df = pickle.load(open('popular.pkl','rb'))
+pt = pickle.load(open('pt.pkl','rb'))
+books = pickle.load(open('books.pkl','rb'))
+similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           book_name = list(popular_df['Book-Title'].values),
+                           author=list(popular_df['Book-Author'].values),
+                           image=list(popular_df['Image-URL-M'].values),
+                           votes=list(popular_df['num_ratings'].values),
+                           rating=list(popular_df['avg_ratings'].values)
+                           )
+
+@app.route('/recommend')
+def recommend_ui():
+    return render_template('recommend.html')
+
+@app.route('/recommend_books', methods=['post'])
+def recommend():
+    user_input = request.form.get('user_input').strip().lower()  # Normalize input
+    pt.index = pt.index.str.strip().str.lower()  # Normalize index
+    
+    if user_input not in pt.index:
+        print(f"User Input: '{user_input}' not found in pt.index")  # Debugging line
+        return render_template('recommend.html', data=[['Sorry this book is not available in database', '', '']])
+    
+    index = np.where(pt.index == user_input)[0][0]
+    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
+
+    data = []
+    for i in similar_items:
+        item = []
+        temp_df = books[books['Book-Title'].str.lower() == pt.index[i[0]]]
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+
+        data.append(item)
+
+    print(f"Recommendations Data: {data}")  # Debugging line to check data
+    
+    return render_template('recommend.html', data=data)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
